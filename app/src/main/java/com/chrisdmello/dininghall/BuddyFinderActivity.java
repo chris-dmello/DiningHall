74e30fc03873675202bf0765756f26355de82204
rakeen01 <rakeen.murtaza.12@gmail.com>
2019-02-23 21:00:28 -0600

    New message

--- app/src/main/java/com/chrisdmello/dininghall/BuddyFinderActivity.java
+++ app/src/main/java/com/chrisdmello/dininghall/BuddyFinderActivity.java
@@ -9,5 +9,7 @@ public class BuddyFinderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buddy_finder);
+
+        // Hello
     }
 }
